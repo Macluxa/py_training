1e7f44f3db8f46f17f8d18653742ccbb5aa10324
Macluxa <macluxa.m@gmail.com>
2016-04-04 08:56:17 +0300

    func arg

--- 05.py
+++ 05.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
 
+def mult(*digits):
+    result = 1
+    for d in digits:
+        result *= d
+    return result
+
+print(mult(10, 20 ,30, 40))
+
+def print_dict(**kwargs):
+    for item in kwargs.items():
+        print(*item, sep=": ")
+
+print_dict(last_name='Ivan', first_name='Petrov')
